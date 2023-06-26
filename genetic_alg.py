12fa2722a53bbe6f1504406c2c974002c127a402
paulo <costa.paulo@unifesp.br>
2023-06-26 12:17:09 -0300

    UMDA-v1

--- genetic_alg.py
+++ genetic_alg.py
@@ -1,44 +1,26 @@
 import random
+import numpy as np
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import glob
-import numpy as np
 
 
 def generate_individual(num_features):
-    """
-    Generate a random individual
-    """
     individual = [random.randint(0, 1) for _ in range(num_features)]
     while sum(individual) == 0:
         individual = [random.randint(0, 1) for _ in range(num_features)]
-
     return individual
 
 
 def generate_population(population_size, num_features):
-    """
-    Generate a population of 'population_size' individuals.
-    """
     return [generate_individual(num_features) for _ in range(population_size)]
 
 
 def calculate_fitness(individual, X_train, X_test, y_train, y_test):
-    """
-    Calculate the fitness of an individual by training an SVM classifier and
-    evaluating its performance using accuracy.
-    """
-    # print(f'individual: {individual}')
-    selected_bands = list(np.nonzero(individual)[0])
-
-    X_train_sel = [
-        segment[selected_bands, :, :].reshape(-1) for segment in X_train
-    ]
-    # print(f'X_train_sel[0].shape: {X_train_sel[0].shape}')
-    X_test_sel = [
-        segment[selected_bands, :, :].reshape(-1) for segment in X_test
-    ]
+    selected_features = [i for i, val in enumerate(individual) if val == 1]
+    X_train_sel = X_train[:, selected_features]
+    X_test_sel = X_test[:, selected_features]
 
     clf = SVC()
     clf.fit(X_train_sel, y_train)
@@ -47,9 +29,6 @@ def calculate_fitness(individual, X_train, X_test, y_train, y_test):
 
 
 def evaluate_population(population, X_train, X_test, y_train, y_test):
-    """
-    Evaluate the fitness of each individual in the population.
-    """
     return [
         calculate_fitness(individual, X_train, X_test, y_train, y_test)
         for individual in population
@@ -57,128 +36,61 @@ def evaluate_population(population, X_train, X_test, y_train, y_test):
 
 
 def select_individuals(population, scores, num_parents):
-    """
-    Select the top 'num_parents' individuals based on their fitness scores.
-    """
-    selected_parents = []
-    sorted_indices = sorted(range(len(scores)),
-                            key=lambda k: scores[k],
-                            reverse=True)
-
-    for i in range(num_parents):
-        selected_parents.append(population[sorted_indices[i]])
-
-    return selected_parents
+    sorted_indices = sorted(range(len(scores)), key=lambda k: scores[k], reverse=True)
+    return [population[i] for i in sorted_indices[:num_parents]]
 
 
-def crossover(parents, offspring_size):
-    """
-    Perform crossover to create 'offspring_size' new individuals.
-    """
+def estimate_distribution(selected_individuals, num_features):
+    distribution = np.zeros(num_features)
+    for individual in selected_individuals:
+        distribution += np.array(individual)
+    return distribution / len(selected_individuals)
+
+
+def generate_offspring(parents, num_offspring, distribution):
     offspring = []
-
-    for _ in range(offspring_size):
-        parent1 = random.choice(parents)
-        parent2 = random.choice(parents)
-
-        half = len(parent1) // 2
-        offspring.append(parent1[:half] + parent2[half:])
-
+    for _ in range(num_offspring):
+        individual = [int(np.random.uniform() < prob) for prob in distribution]
+        offspring.append(individual)
     return offspring
 
 
-def mutation(offspring):
-    """
-    Perform mutation by flipping a random bit in each individual.
-    """
-    mutated_offspring = []
-
-    for individual in offspring:
-        index = random.randint(0, len(individual) - 1)
-        individual[index] = 1 - individual[index]
-        mutated_offspring.append(individual)
-
-    return mutated_offspring
-
-
-def genetic_algorithm(X_train,
-                      X_test,
-                      y_train,
-                      y_test,
-                      population_size,
-                      num_generations,
-                      num_parents,
-                      num_offspring,
-                      num_best=5):
-    num_features = X_train[0].shape[0]
+def genetic_algorithm(X_train, X_test, y_train, y_test, population_size,
+                      num_generations, num_parents, num_offspring):
+    num_features = X_train.shape[1]
     population = generate_population(population_size, num_features)
 
     for generation in range(num_generations):
-        print("Generation", generation + 1)
-
-        # Evaluate fitness
-        scores = evaluate_population(population, X_train, X_test, y_train,
-                                     y_test)
-
-        # Select parents
+        scores = evaluate_population(population, X_train, X_test, y_train, y_test)
         parents = select_individuals(population, scores, num_parents)
-
-        # Create offspring through crossover
-        offspring = crossover(parents, num_offspring)
-
-        # Perform mutation
-        offspring = mutation(offspring)
-
-        # Replace the population with offspring
+        distribution = estimate_distribution(parents, num_features)
+        offspring = generate_offspring(parents, num_offspring, distribution)
         population = parents + offspring
 
-    # Get the final fitness scores
     scores = evaluate_population(population, X_train, X_test, y_train, y_test)
+    best_individual_index = np.argmax(scores)
+    best_individual = population[best_individual_index]
+    best_fitness = scores[best_individual_index]
 
-    # print(f'scores: {scores}')
-
-    # Select the best individuals
-    best_indices = sorted(range(len(scores)),
-                          key=lambda k: scores[k],
-                          reverse=True)[:num_best]
-    best_individuals = [population[i] for i in best_indices]
-    best_fitnesses = [scores[i] for i in best_indices]
-
-    return best_individuals, best_fitnesses
+    return best_individual, best_fitness
 
 
 # Loading dataset
 X_all = []
 y_all = []
-for path in glob.glob('data/dataset_v2/forest/*'):
+for path in glob.glob('dataset_v2/forest/*'):
     X_all.append(np.load(path))
     y_all.append(0)
 
-for path in glob.glob('data/dataset_v2/non_forest/*'):
+for path in glob.glob('dataset_v2/non_forest/*'):
     X_all.append(np.load(path))
     y_all.append(1)
 
-# perform split
+# Perform train-test split
 X_train, X_val, y_train, y_val = train_test_split(X_all, y_all, test_size=0.4)
 X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=0.5)
 
-# call the genetic algorithm
-num_best = 5
+# Call the genetic algorithm
 population_size = 50
 num_generations = 10
-num_parents = 15
-num_offspring = 20
-
-best_individuals, best_fitnesses = genetic_algorithm(X_train, X_val, y_train,
-                                                     y_val, population_size,
-                                                     num_generations,
-                                                     num_parents,
-                                                     num_offspring)
-
-for i in range(num_best):
-    print(f"Individual #{i+1}: {best_individuals[i]}")
-    print(f"With fitness: {best_fitnesses[i] :.3f}")
-    test_result = calculate_fitness(best_individuals[i], X_train, X_test,
-                                    y_train, y_test)
-    print(f'And test accuracy: {test_result :.3f}')
-    print()
+num
