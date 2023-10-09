0f65fb3f1510c021431e917e409ed7a0b1f05189
ebouhid <eduardobouhid.neto@gmail.com>
2023-10-09 15:42:47 -0300

    included grid/randomized search for SVM classifier on fitness function

--- umda.py
+++ umda.py
@@ -8,6 +8,7 @@ import pandas as pd
 from tqdm import tqdm
 import sys
 import logging
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
 # Get command line arguments
 exp_name = str(sys.argv[1])
@@ -33,7 +34,7 @@ def calculate_fitness(individual, X_train, X_test, y_train, y_test):
     Calculate the fitness of an individual by training an SVM classifier and
     evaluating its performance using accuracy.
     """
-    # print(f'individual: {individual}')
+    print(f'individual: {individual}')
     selected_bands = list(np.nonzero(individual)[0])
 
     X_train_sel = [
@@ -44,14 +45,39 @@ def calculate_fitness(individual, X_train, X_test, y_train, y_test):
         segment[selected_bands, :, :].reshape(-1) for segment in X_test
     ]
 
-    clf = SVC(C=100,
-              gamma='scale',
-              kernel='rbf',
-              class_weight='balanced',
-              random_state=seed)
-    clf.fit(X_train_sel, y_train)
-    y_pred = clf.predict(X_test_sel)
-    return balanced_accuracy_score(y_test, y_pred)
+    # Create an SVM classifier
+    clf = SVC(random_state=seed)
+
+    # Define a grid of hyperparameter values
+    param_dist = {
+        'C': [0.1, 1, 10, 100],
+        'gamma': ['scale', 'auto', 0.001, 0.01, 0.1, 1],
+        'kernel': ['linear', 'rbf'],
+        'class_weight': [None, 'balanced']
+    }
+
+    # Perform a randomized search with cross-validation (you can use GridSearchCV as well)
+    random_search = RandomizedSearchCV(
+        clf,
+        param_distributions=param_dist,
+        n_iter=10,  # Adjust the number of iterations as needed
+        cv=5,
+        scoring='balanced_accuracy',
+        random_state=seed,
+        verbose=4)
+    random_search.fit(X_train_sel, y_train)
+
+    # Get the best SVM classifier from the randomized search
+    best_clf = random_search.best_estimator_
+
+    # Fit the best classifier on the training data and make predictions
+    best_clf.fit(X_train_sel, y_train)
+    y_pred = best_clf.predict(X_test_sel)
+
+    # Calculate the balanced accuracy score for the best classifier
+    balanced_acc = balanced_accuracy_score(y_test, y_pred)
+
+    return balanced_acc
 
 
 def evaluate_population(population, X_train, X_test, y_train, y_test):
@@ -220,8 +246,8 @@ population_size = 10
 num_generations = 10
 num_parents = 5
 num_offspring = 5
-inf_lim = 0.00
-sup_lim = 1.00
+inf_lim = 0.10
+sup_lim = 0.87
 mut_prob = 0
 xover_prob = 0
 
