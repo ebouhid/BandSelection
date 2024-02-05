2f565d8c2490c9321505433cecc14891470995ce
ebouhid <ebneto@unifesp.br>
2024-02-05 07:43:48 -0300

    Generate confusion mask for each patch and write its metrics to it.

--- segmentation/get_metrics.py
+++ segmentation/get_metrics.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 if __name__ == '__main__':
     regions = ["x03", "x04"]
@@ -20,23 +21,26 @@ if __name__ == '__main__':
         "4317": [4, 3, 1, 7],
         "4316": [4, 3, 1, 6],
         "43167": [4, 3, 1, 6, 7],
-        # "PCA": range(1, 4)
     }
 
     # Convert to 0-based indexing
     for composition in compositions:
         compositions[composition] = [x - 1 for x in compositions[composition]]
 
+    # Create a directory to store the confusion masks
+    os.makedirs('outputs/confusion_masks', exist_ok=True)
+
     # Open a .txt file to store the results in a LaTeX table
     with open('outputs/latex_table.txt', 'w') as f:
         f.write("\\textbf{Composition} & \\textbf{Precision} & \\textbf{Recall} & \\textbf{F1-score} & \\textbf{Accuracy} & \\textbf{IoU} & \\textbf{Epoch}\\ \hline\n")
-        for composition in compositions:
-            precision_list = []
-            recall_list = []
-            f1_list = []
-            accuracy_list = []
-            iou_list = []
-            for region in regions:
+        for region in regions:
+            df = []
+            for composition in compositions:
+                precision_list = []
+                recall_list = []
+                f1_list = []
+                accuracy_list = []
+                iou_list = []
 
                 if composition == "PCA":
                     image = Image.open(f'dataset/scenes_pca/pca_{region}.png')
@@ -48,13 +52,18 @@ if __name__ == '__main__':
                 
                 height, width, _ = image.shape
 
-                model = torch.load(f'models/UMDA_Compositions-DeepLabV3Plus-{composition}.pth')
+                model = torch.load(f'models/UMDA_Compositions_Imagenet-DeepLabV3Plus-{composition}.pth')
 
                 model.eval()
 
                 predicted_masks = []
                 patch_counts = []
+                if region == "x03":
+                    paddedsize = (1024, 1024)
+                if region == "x04":
+                    paddedsize = (1024, 768)
 
+                confusion_mask_scene = np.zeros((paddedsize[0], paddedsize[1], 3), dtype=np.uint8)
                 # Iterate through patches
                 for y in range(0, height, stride):
                     for x in range(0, width, stride):
@@ -93,6 +102,16 @@ if __name__ == '__main__':
                         true_negatives = np.logical_and(truth_mask_patch == 0, predicted_mask == 0)
                         false_positives = np.logical_and(truth_mask_patch == 0, predicted_mask == 1)
                         false_negatives = np.logical_and(truth_mask_patch == 1, predicted_mask == 0)
+                        
+                        # Create confusion mask for patch
+                        confusion_mask = np.zeros((patch_size[1], patch_size[0], 3), dtype=np.uint8)
+                        confusion_mask[true_positives] = [255, 255, 255]
+                        confusion_mask[true_negatives] = [0, 0, 0]
+                        confusion_mask[false_positives] = [0, 0, 255]
+                        confusion_mask[false_negatives] = [255, 0, 0]
+
+                        # Confusion mask for whole scene
+                        confusion_mask_scene[y:y+patch_size[1], x:x+patch_size[0], :] = confusion_mask
 
                         # Calculate the number of true positives, true negatives, false positives, and false negatives
                         tp = np.sum(true_positives)
@@ -117,34 +136,45 @@ if __name__ == '__main__':
                         recall_list.append(recall)
                         f1_list.append(f1_score)
                         iou_list.append(iou)
+
+                        # Write metrics for this patch on the image
+                        plt.imshow(confusion_mask)
+                        plt.text(10, 10, f'Accuracy: {accuracy:.2f}', color='lime')
+                        plt.text(10, 30, f'Precision: {precision:.2f}', color='lime')
+                        plt.text(10, 50, f'Recall: {recall:.2f}', color='lime')
+                        plt.text(10, 70, f'F1: {f1_score:.2f}', color='lime')
+                        plt.text(10, 90, f'IoU: {iou:.2f}', color='lime')
+                        plt.savefig(f'outputs/confusion_masks/confusion_{composition}_{region}_{x}_{y}.png')
+                        plt.clf()
         
-            accuracy_avg = np.mean(accuracy_list)
-            precision_avg = np.mean(precision_list)
-            recall_avg = np.mean(recall_list)
-            f1_avg = np.mean(f1_list)
-            iou_avg = np.mean(iou_list)
+                accuracy_avg = np.mean(accuracy_list)
+                precision_avg = np.mean(precision_list)
+                recall_avg = np.mean(recall_list)
+                f1_avg = np.mean(f1_list)
+                iou_avg = np.mean(iou_list)
 
-            # print(f"Composition: {composition}")
-            # print(f'Accuracy list: {accuracy_list}')
-            # print(f'Precision list: {precision_list}')
-            # print(f'Recall list: {recall_list}')
-            # print(f'F1 list: {f1_list}')
-            # print(f'IoU list: {iou_list}')
-            print(f'total patches: {len(accuracy_list)}')
+                print(f'total patches: {len(iou_list)}')
+                # # Save the confusion mask for the whole scene
+                # os.makedirs('outputs/big_confusion_masks', exist_ok=True)
+                # confusion_mask_scene = Image.fromarray(confusion_mask_scene)
+                # confusion_mask_scene.save(f'outputs/big_confusion_masks/bigconfusion_{composition}_{region}.png')
+                assert iou_avg == np.sum(iou_list) / len(iou_list)
 
-            # Write in latex format
-            f.write(f"{composition} & {precision_avg * 100:.2f} & {recall_avg * 100:.2f} & {f1_avg * 100:.2f} & {accuracy_avg * 100:.2f} & {iou_avg * 100:.2f} \\ \hline \n")
+            # # Write in latex format
+            # f.write(f"{composition} & {precision_avg * 100:.2f} & {recall_avg * 100:.2f} & {f1_avg * 100:.2f} & {accuracy_avg * 100:.2f} & {iou_avg * 100:.2f} \\ \hline \n")
 
-            # Also add to a pandas dataframe
-            df.append({
-                "Composition": composition,
-                "Precision": precision_avg * 100,
-                "Recall": recall_avg * 100,
-                "F1": f1_avg * 100,
-                "Accuracy": accuracy_avg * 100,
-                "IoU": iou_avg * 100
-            })
+                # Also add to a pandas dataframe
+                df.append({
+                    "Region": region,
+                    "Composition": composition,
+                    "Precision": f'{precision_avg * 100 :.2f}',
+                    "Recall": f'{recall_avg * 100 :.2f}',
+                    "F1": f'{f1_avg * 100 :.2f}',
+                    "Accuracy": f'{accuracy_avg * 100 :.2f}',
+                    "IoU": f'{iou_avg * 100 :.2f}'
+                })
 
-        df = pd.DataFrame(df)
-        df = df.sort_values(by="IoU", ascending=False)  
-        df.to_excel("outputs/results_byhand.xlsx", index=False)
+            df = pd.DataFrame(df)
+            df = df.sort_values(by=["IoU"], ascending=False)
+            print(df)
+            df.to_excel(f"outputs/results_byhand_{region}.xlsx", index=False)
