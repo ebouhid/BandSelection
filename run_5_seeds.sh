49d25dda8e88b3e60ef02c99ff337308ea48ce4b
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2024-03-21 08:38:56 -0300

    Changed run 5 seeds script to use multiple GPUS. TODO: make this more versatile; specify number of GPUS and distribute training according to it.

--- run_5_seeds.sh
+++ run_5_seeds.sh
@@ -2,19 +2,19 @@ export EXP_NAME=$1
 export GPU_ID=$2
 
 mkdir results/$EXP_NAME
-nohup python umda.py $EXP_NAME 1 $GPU_ID> results/$EXP_NAME/genetic-1.out 2>&1 &
+nohup python umda.py $EXP_NAME 1 0> results/$EXP_NAME/genetic-1.out 2>&1 &
 nohup python bruteforce.py $EXP_NAME 1 > results/$EXP_NAME/bruteforce-1.out 2>&1 &
 
-nohup python umda.py $EXP_NAME 10 $GPU_ID > results/$EXP_NAME/genetic-10.out 2>&1 &
+nohup python umda.py $EXP_NAME 10 0 > results/$EXP_NAME/genetic-10.out 2>&1 &
 nohup python bruteforce.py $EXP_NAME 10 > results/$EXP_NAME/bruteforce-10.out 2>&1 &
 
-nohup python umda.py $EXP_NAME 20 $GPU_ID> results/$EXP_NAME/genetic-20.out 2>&1 &
+nohup python umda.py $EXP_NAME 20 0> results/$EXP_NAME/genetic-20.out 2>&1 &
 nohup python bruteforce.py $EXP_NAME 20 > results/$EXP_NAME/bruteforce-20.out 2>&1 &
 
-nohup python umda.py $EXP_NAME 30 $GPU_ID> results/$EXP_NAME/genetic-30.out 2>&1 &
+nohup python umda.py $EXP_NAME 30 1> results/$EXP_NAME/genetic-30.out 2>&1 &
 nohup python bruteforce.py $EXP_NAME 30 > results/$EXP_NAME/bruteforce-30.out 2>&1 &
 
-nohup python umda.py $EXP_NAME 42 $GPU_ID> results/$EXP_NAME/genetic-42.out 2>&1 &
+nohup python umda.py $EXP_NAME 42 1> results/$EXP_NAME/genetic-42.out 2>&1 &
 nohup python bruteforce.py $EXP_NAME 42 > results/$EXP_NAME/bruteforce-42.out 2>&1 &
 
 echo 'Job submitted'
