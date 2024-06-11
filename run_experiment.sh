8b0f6b941dbdf3629bcb8ab64270e24c6e9d41f5
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2024-06-11 00:22:52 -0300

    Removed deprecated 'run_experiment.sh' script

--- run_experiment.sh
+++
@@ -1,5 +0,0 @@
-export EXP_NAME=$1
-mkdir results/$EXP_NAME
-nohup python genetic_alg.py $EXP_NAME 42 > results/$EXP_NAME/genetic.out 2>&1 &
-nohup python bruteforce.py $EXP_NAME 42 > results/$EXP_NAME/bruteforce.out 2>&1 &
-echo 'Job submitted'
